682894d9b7f72ed4a281aca5cd1aafb10d60ec7e
Chloe M <chloe.molle@neuf.fr>
2025-02-13 09:51:26 +0100

    fix script

--- survey/handleData.js
+++ survey/handleData.js
@@ -5,10 +5,8 @@ import { handleInputExcel, initOutputExcel } from './helpers/utils.js';
 const { inputworksheet, numberOfRow } = await handleInputExcel();
 const { outputWorkbook, outputWorksheet, headerRow } = await initOutputExcel();
 
-console.log(numberOfRow);
 const engine = new Engine(rules);
 for (let i = 2; i <= numberOfRow; i ++) {
-    console.log(i)
     const headerInput = inputworksheet.getRow(1).values;
     const currentRow = inputworksheet.getRow(i).values;
     const situation = {};
@@ -18,18 +16,23 @@ for (let i = 2; i <= numberOfRow; i ++) {
             if (currentRow[headerIndex] === true || currentRow[headerIndex] === false) {
                 situation[headerInput[headerIndex].trim()] = currentRow[headerIndex] === true ? "oui" : "non";
             } else if (currentRow[headerIndex] !== 'je ne sais pas') {
-                situation[headerInput[headerIndex].trim()] = currentRow[headerIndex];
+                if (typeof currentRow[headerIndex] === 'string') {
+                    situation[headerInput[headerIndex].trim()] = `'${currentRow[headerIndex]}'`;
+                } else {
+                    situation[headerInput[headerIndex].trim()] = currentRow[headerIndex];
+                }
             }
         }
     }
 
-    engine.setSituation(situation);
+    engine.setSituation({ ...situation, 'transport . deux roues . type': "'moto inf 250'"});
     const computeRow = [currentRow[1]];
 
     for (let ruleIndex = 2; ruleIndex <= headerRow.length; ruleIndex ++) {
         const rule = headerRow[ruleIndex];
-        if (rule) computeRow.push(engine.evaluate(rule).nodeValue);
-        else computeRow.push(0);
+        if (rule) {
+            computeRow.push(engine.evaluate(rule).nodeValue);
+        } else computeRow.push(0);
     }
 
     outputWorksheet.addRow(computeRow);
